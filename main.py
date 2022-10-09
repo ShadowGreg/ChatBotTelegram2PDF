acf4060093ce5497f45e806104b400851e8aa2f8
Alexander Konukhov <jesusmisha@gmail.com>
2022-10-09 14:52:20 +0300

    update main: folder of the file, clearing directory after conversion

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from start_bot import bot
 from clear_catalog import clear_catalog
 from txt_to_pdf import convert_text_pdf
 from excel_to_pdf import excel_to_pdf
+from picture_to_pdf import img_2_pdf
+from datetime import datetime
 
 local_src = ""
 SRC = './tmp_files/'
@@ -33,20 +35,20 @@ def handle_docs(message):
         real_file_name, real_file_extension = os.path.splitext(message.document.file_name)
         file_name = real_file_name.lower()
         file_extension = real_file_extension.lower()
+        # TODO: доделать так что бы сначала была папку с чат айди потом внутри папки с оригиналами файлов
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = SRC + file_name + '_' + str(chat_id) + '_' + str(datetime.today().strftime('%Y-%m-%d %H-%M-%S'))
+        # создаем папку в которой будем временно размещать файл, если таковой не существует
+        if not os.path.exists(src):
+            os.makedirs(src)
+        # создаем путь конечного файла - думаю надо переделать - это временный вариант
+        local_src = src + '/' + real_file_name + real_file_extension
+        # пишем файл на диск
+        with open(local_src, 'wb') as new_file:
+            new_file.write(downloaded_file)
 
         if file_extension == '.txt':  # проверяем расширение txt
-            # TODO: доделать так что бы сначала была папку с чат айди потом внутри папки с оригиналами файлов
-            file_info = bot.get_file(message.document.file_id)
-            downloaded_file = bot.download_file(file_info.file_path)
-            src = SRC + file_name + '_' + str(chat_id) + '_' + str(os.times().system)
-            # создаем папку в которой будем временно размещать файл, если таковой не существует
-            if not os.path.exists(src):
-                os.makedirs(src)
-            # создаем путь конечного файла
-            local_src = src + '/' + real_file_name + real_file_extension
-            # пишем файл на диск
-            with open(local_src, 'wb') as new_file:
-                new_file.write(downloaded_file)
             bot.reply_to(message, "Конвертирую 😉")
             convert_text_pdf(local_src)
             send_document(convert_text_pdf(local_src), chat_id)
@@ -54,11 +56,12 @@ def handle_docs(message):
             bot.reply_to(message, "xls")
         elif file_extension == '.doc' or '.docx':  # проверяем расширение excel
             bot.reply_to(message, "doc")
+        elif file_extension == '.jpg' or '.jpeg' or '.png' or '.tiff' or '.jpg2' or '.heif' or '.heic':
+            # отсылаем файл пользователю (используем модуль конвертера)
+            send_document(img_2_pdf(local_src, message), chat_id)
         else:
             bot.reply_to(message, f"я не знаю такого '{file_extension}' формата 😶‍🌫️😇")
-
-        clear_catalog(
-            SRC)  # ВНИМАНИЕ! теперь функция удаляет и файлы и папки - пути писать аккуратно что бы не затерло системные файлы
+        clear_catalog(src)  # ВНИМАНИЕ! теперь функция удаляет и файлы и папки - пути писать аккуратно что бы не затерло системные файлы
     except Exception as e:
         bot.reply_to(message, e)
 
@@ -72,4 +75,3 @@ def echo(message):
 
 
 bot.polling(none_stop=True, interval=0)
-# important change
