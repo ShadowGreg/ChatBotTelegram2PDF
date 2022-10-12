8c04cbbb3144c1069ad937dc194499093464338c
ShadowGreg <shadow-greg@yandex.ru>
2022-10-12 20:02:39 +0300

    fix add compress image converting

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def handle_docs(message):
         chat_id = message.chat.id
         # получаем имя и расширение файла, так что бы пронести переменные до конца
         get_object = message.document  # получаемый объект
-        real_file_name, real_file_extension = os.path.splitext(get_object.file_name)  # бот не понимает картинку
+        real_file_name, real_file_extension = os.path.splitext(get_object.file_name)
         file_name = real_file_name.lower()
         file_extension = real_file_extension.lower()
         file_info = bot.get_file(get_object.file_id)
@@ -63,39 +63,55 @@ def handle_docs(message):
         # пишем файл на диск
         with open(local_src, 'wb') as new_file:
             new_file.write(downloaded_file)
-        if file_extension == '.txt':  # проверяем расширение txt
-            bot.reply_to(message, "Конвертирую ⚙️⚙")  # Нужно ли это писать? Выглядит перебором! Предлагаю минимализм
-            convert_text_pdf(local_src)  # и весь лишний вывод вообще убрать. Оставим пока так, для наглядности.
-            send_document(convert_text_pdf(local_src), chat_id)
-        elif file_extension == '.xls' \
-                or file_extension == '.xlsx':  # проверяем расширение excel
-            bot.reply_to(message, "xls")
-        elif file_extension == '.doc' \
-                or file_extension == '.docx':  # проверяем расширение doc
-            bot.reply_to(message, "doc")
-        elif file_extension == '.jpg' \
-                or file_extension == '.jpeg' \
-                or file_extension == '.png' \
-                or file_extension == '.tiff' \
-                or file_extension == '.jpg2' \
-                or file_extension == '.heif' \
-                or file_extension == '.heic':  # картинок
-            # отсылаем файл пользователю (используем модуль конвертера)
-            bot.reply_to(message, "Конвертирую картинку в pdf ⚙️⚙️")
-            img_2_pdf(local_src)
-            send_document(img_2_pdf(local_src), chat_id)
-        else:
-            bot.reply_to(message, f"я не знаю такого '{file_extension}' формата 😶‍🌫️😇")
-        clear_catalog(
-            src)  # ВНИМАНИЕ!
-        # теперь функция удаляет и файлы и папки - пути писать аккуратно что бы не затерло системные файлы
+        file_switcher(chat_id, file_extension, local_src, message, src)
     except Exception as e:
         bot.reply_to(message, e)
 
 
+def file_switcher(chat_id, file_extension, local_src, message, src):
+    if file_extension == '.txt':  # проверяем расширение txt
+        bot.reply_to(message, "Конвертирую ⚙️⚙")  # Нужно ли это писать? Выглядит перебором! Предлагаю минимализм
+        convert_text_pdf(local_src)  # и весь лишний вывод вообще убрать. Оставим пока так, для наглядности.
+        send_document(convert_text_pdf(local_src), chat_id)
+    elif file_extension == '.xls' \
+            or file_extension == '.xlsx':  # проверяем расширение excel
+        bot.reply_to(message, "xls")
+    elif file_extension == '.doc' \
+            or file_extension == '.docx':  # проверяем расширение doc
+        bot.reply_to(message, "doc")
+    elif file_extension == '.jpg' \
+            or file_extension == '.jpeg' \
+            or file_extension == '.png' \
+            or file_extension == '.tiff' \
+            or file_extension == '.jpg2' \
+            or file_extension == '.heif' \
+            or file_extension == '.heic':  # картинок
+        # отсылаем файл пользователю (используем модуль конвертера)
+        bot.reply_to(message, "Конвертирую картинку в pdf ⚙️⚙️")
+        img_2_pdf(local_src)
+        send_document(img_2_pdf(local_src), chat_id)
+    else:
+        bot.reply_to(message, f"я не знаю такого '{file_extension}' формата 😶‍🌫️😇")
+        # теперь функция удаляет и файлы и папки - пути писать аккуратно что бы не затерло системные файлы
+    clear_catalog(src)  # ВНИМАНИЕ!
+
+
 @bot.message_handler(content_types=['photo'])
 def photo(message):
-    bot.send_message(message, 'фото')
+    chat_id = message.chat.id
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    file_extension = '.'+bot.get_file(file_id).file_path.split('.')[1]
+    downloaded_file = bot.download_file(file_info.file_path)
+    src = SRC + str(chat_id) + '_' + str(datetime.today().strftime('%Y%m%d%H%M%S'))
+    # создаем папку в которой будем временно размещать файл, если таковой не существует
+    if not os.path.exists(src):
+        os.makedirs(src)
+    # создаем путь конечного файла - думаю надо переделать - это временный вариант
+    local_src = src + '/' + 'image'+ file_extension
+    with open(local_src, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    file_switcher(chat_id, file_extension, local_src, message, src)
 
 
 @bot.message_handler(func=lambda message: True)  # Бот на любое сообщение пользователя, кроме файла
