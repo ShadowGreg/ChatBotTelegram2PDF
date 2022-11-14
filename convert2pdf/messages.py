aff2809abf891422a6fef00ea448e68cc1c90483
Pavel Tsypyshev <ptsypyshev@gmail.com>
2022-11-14 15:56:46 +0500

    Added auto-detecting for txt charset Fixed incorrect csv2pdf encoding

--- convert2pdf/messages.py
+++ convert2pdf/messages.py
@@ -38,7 +38,7 @@ CONVERT_MESSAGE = 'Конвертирую, это может занять нек
 HTML_HEAD = '''<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="$enc">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
